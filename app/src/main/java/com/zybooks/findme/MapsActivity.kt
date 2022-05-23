54a36881c34773774e12ceb6d7ff6c39356d6f9c
Luftwaffle3 <reedreed2000@gmail.com>
2022-05-23 08:33:06 -0700

    Initial commit

---
+++ app/src/main/java/com/zybooks/findme/MapsActivity.kt
@@ -0,0 +1,112 @@
+package com.zybooks.findme
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.*
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.zybooks.findme.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var googleMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+    private var client: FusedLocationProviderClient? = null
+    private var locationRequest: LocationRequest? = null
+    private var locationCallback: LocationCallback? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        if (hasLocationPermission()) {
+            trackLocation()
+        }
+    }
+
+    private fun trackLocation() {
+
+        // Create location request
+        locationRequest = LocationRequest.create()
+            .setInterval(5000)
+            .setFastestInterval(3000)
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+
+        // Create location callback
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+                for (location in locationResult.locations) {
+                    updateMap(location)
+                }
+            }
+        }
+
+        client = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    private fun updateMap(location: Location) {
+
+        // TODO: Show user's location on the map
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        this.googleMap = googleMap
+    }
+
+    override fun onPause() {
+        super.onPause()
+        client?.removeLocationUpdates(locationCallback!!)
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onResume() {
+        super.onResume()
+        if (hasLocationPermission()) {
+            client?.requestLocationUpdates(
+                locationRequest!!, locationCallback!!, Looper.getMainLooper())
+        }
+    }
+
+    private fun hasLocationPermission(): Boolean {
+
+        // Request fine location permission if not already granted
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_DENIED
+        ) {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            return false
+        }
+        return true
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            trackLocation()
+        }
+    }
+}
